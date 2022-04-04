a4e1229ebd67ff4da36c190e1e5031256bdfe2d3
tmyung <74217554+tmyung@users.noreply.github.com>
2022-04-04 15:23:54 -0700

    Initial Commit
    Created REACT app called uniilanguage and added minimal src code

---
+++ uniilanguage/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export default function App() {
+    return (
+        <>
+            <h1 class="logo">
+                <span style={{color: "red"}}>U</span>
+                <span style={{color: "yellow"}}>n</span>
+                <span style={{color: "green"}}>i</span>
+                <span style={{color: "skyblue"}}>i</span>
+                <span style={{color: "black"}}>language</span>
+            </h1>
+
+            <div>
+
+            </div>
+        </>
+    );
+}
