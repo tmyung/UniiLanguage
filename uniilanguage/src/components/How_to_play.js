3739dcf05814f5e64d51acd894e67b47a1187d44
tmyung <74217554+tmyung@users.noreply.github.com>
2022-04-06 20:53:12 -0700

    renamed App.css to Home.css
    for consistency... ALSO IMA FUCKIN GET THIS NAVIGATION TO WORK CAUSE ITS PISSING ME OFF

--- uniilanguage/src/components/How_to_play.js
+++ uniilanguage/src/components/How_to_play.js
@@ -1,9 +1,14 @@
 import React from "react";
+import './App.css';
 
 const How_to_play = () => {
     return(
         <>
-
+            <div>
+                <h1>
+                    test
+                </h1>
+            </div>
         </>
     );
 }
