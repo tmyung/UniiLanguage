cb5d5ba3d2be5b396b09a4187f34cceba235ba48
Alina Kim <Squeakadoodle6084@gmail.com>
2022-05-18 00:38:54 -0700

    Added prompt generation functionality

--- uniilanguage/prompt generation/prompt_generator.js
+++ uniilanguage/prompt generation/prompt_generator.js
@@ -1,14 +1,35 @@
-function getFileContent(file) {
-	var text = "";
-	var reader = new FileReader();
-	reader.onload = function() {
-		text = reader.result;
-	}
-	reader.readAsText(file);
-	return text;
+async function alertTest() {
+	alert(prek3);
 }
 
-async function alertContent() {
-	let blob = await fetch("./prek3.csv").then(r => r.blob());
-	alert(getFileContent(blob));
+function generatePrompt(promptset) {
+	let format = String(randomSelect(promptset.get('Formats')));
+	let resolved = "";
+	while( format.length > 0 ) {
+		while( !format.startsWith('%') ) {
+			if( format.length < 1 ) {
+				break;
+			}
+			resolved = resolved.concat(format[0]);
+			format = format.slice(1);
+		}
+		if( format.length < 1 ) {
+			break;
+		}
+		format = format.slice(1);
+		let ei = format.indexOf('%');
+		let placeholder = format.slice(0,ei);
+		format = format.slice(ei+1);
+		let substitution = resolveFormat(placeholder, promptset);
+		resolved = resolved.concat(substitution);
+	}
+	return resolved;
+}
+
+function resolveFormat(placeholder, promptset) {
+	return randomSelect(promptset.get(placeholder));
+}
+
+function randomSelect(options) {
+	return options[Math.floor(Math.random() * options.length)];
 }
